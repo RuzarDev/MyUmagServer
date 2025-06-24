7a4c6f9ed328e3044bdc64a1c036585dd74bc32f
RuzarDev <urazov.ruzar@inbox.ru>
2025-06-24 21:33:10 +0500

    fix

--- src/ingridients/tech_card_ingredients.entity.ts
+++ src/ingridients/tech_card_ingredients.entity.ts
@@ -2,17 +2,18 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { TechCardEntity } from "./tech_cards.entity";
 import { AdminEntity } from "../admin/admin.entity";
 import { MenuEntity } from "../menu/menu.entity";
+import { IngredientsEntity } from "./ingredients.entity";
 
 @Entity('tech_card_ingredients')
 export class TechCardIngredientEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne(() => TechCardEntity, techCard => techCard.ingredients)
-  techCard: TechCardEntity;
+  @ManyToOne(() => MenuEntity, menu => menu.ingredients)
+  menu: MenuEntity;
 
-  @ManyToOne(() => MenuEntity)
-  ingredient: MenuEntity;
+  @ManyToOne(() => IngredientsEntity)
+  ingredient: IngredientsEntity;
 
   @Column({default:0,type:'float'})
 
